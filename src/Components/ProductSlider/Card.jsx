377dbaae7a21b95c40a0f2f12f13cac766904f99
IamShihabPro <mshihab.pro@gmail.com>
2023-09-03 00:02:50 +0600

    project update

--- src/Components/ProductSlider/Card.jsx
+++ src/Components/ProductSlider/Card.jsx
@@ -1,16 +1,71 @@
 import { Rating } from '@smastrom/react-rating';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { FaEye, FaShoppingCart } from 'react-icons/fa';
+import Swal from 'sweetalert2';
+import { AuthContext } from '../../Provider/AuthProvider';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useCart from '../../hooks/useCart';
 
 const Card = ({product}) => {
     // style={{ minWidth: '320px' }}
+    const {_id, name, image, description, price, category, sizes, colors, ratings} = product
+
+    const {user} = useContext(AuthContext)
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const [refetch, isLoading] = useCart()
+
+    const handleAddToCart = product =>{
+        console.log(product);
+
+        if(user && user.email){
+            const orderProduct = {productId : _id, name, category,image , price, description, sizes, colors, ratings, email: user.email}
+            fetch(`${import.meta.env.VITE_API_URL}/carts`,{
+                method:"POST",
+                headers:{'content-type': 'application/json'},
+                body: JSON.stringify(orderProduct)
+            })
+            .then(res => res.json())
+            .then(data =>{
+                if(data.insertedId){
+                    isLoading()
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Cart saved',
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                      refetch
+                }
+            })
+        }
+        else{
+            Swal.fire({
+                title: 'Please Login First',
+                
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login Now'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login', {state: {from: location}})
+                }
+              })
+        }
+
+    }
+
     return (
         <>
-            <div  key={product.id} className='col-span-1 cursor-pointer group bg-white drop-shadow-md rounded-md p-2 my-2 mx-3' style={{ minWidth: '320px' }}>
+            <div  key={_id} className='col-span-1 cursor-pointer group bg-white drop-shadow-md rounded-md p-2 my-2 mx-3' style={{ minWidth: '320px' }}>
                         <div className='flex flex-col gap-2 w-full'>
                             <div className=' aspect-square w-full relative overflow-hidden rounded-md'>
                                 <img className=' object-cover h-full w-full group-hover:scale-110 transition'
-                                src={product.image}
+                                src={image}
                                 alt='Cloths'/>
                                 <div className='absolute top-3 right-3'>
                             
@@ -18,22 +73,22 @@ const Card = ({product}) => {
                 
                             </div>
                 
-                            <div className='font-semibold text-lg'>{product.name}</div>
+                            <div className='font-semibold text-lg'>{name}</div>
 
                             <div className='font-light text-neutral-700'>
-                                {product.description}
+                                {description}
                             </div>
 
                             <div className='flex flex-row items-center justify-between'>
-                                <div className='font-semibold'>$ {product.price}</div>
+                                <div className='font-semibold'>$ {price}</div>
                                 <div>
-                                <Rating style={{ maxWidth: 70 }} value={product.ratings} readOnly />
+                                <Rating style={{ maxWidth: 70 }} value={ratings} readOnly />
                                 </div>
                             </div>
 
                             <div className='flex justify-between items-center mt-1'>
-                                <button className="btn btn-outline btn-sm shadow-md">Add to card</button>
-                                <button className="btn btn-sm text-blue-600 bg-white shadow-md"><FaEye></FaEye></button>
+                                <button onClick={()=> handleAddToCart(product)} className="btn btn-outline btn-sm  shadow-md">Add to card</button>
+                                <Link  to={`/productview/${_id}`}> <button className="btn btn-sm text-blue-600 bg-white shadow-md"> <FaEye></FaEye>  </button> </Link>
                             </div>
                         </div>
                     </div>
